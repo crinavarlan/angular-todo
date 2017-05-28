a1c11e2dcab31c1359be62b1f0a6f0265b836a37
Crina <varlan.crina@yahoo.com>
2017-05-28 22:27:45 +0100

    Add bower_components

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', 'TodoService', 'TodoDirective']);
+angular.module('TodoApp', ['ngRoute', 'angular-storage', 'RouteControllers', 'UserService', 'TodoService', 'TodoDirective']);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
-	$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
+
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
@@ -10,11 +11,20 @@ angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
 		templateUrl: 'templates/register.html',
 		controller: 'RegisterController'
 	})
+	.when('/accounts/login', {
+		templateUrl: 'templates/login.html',
+		controller: 'LoginController'
+	})
+	.when('/accounts/logout', {
+		templateUrl: 'templates/logout.html',
+		controller: 'LogoutController'
+	})
 	.when('/todo', {
 		templateUrl: 'templates/todo.html',
 		controller: 'TodoController'
-	}).when('/todo/edit/:id', {
-	    templateUrl:'templates/edit-todo.html',
-	    controller: 'EditTodoController'
+	})
+	.when('/todo/edit/:id', {
+		templateUrl:'templates/edit-todo.html',
+		controller: 'EditTodoController'
 	});
 });
